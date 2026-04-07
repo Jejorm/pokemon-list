bf0a89e01aa65b85cb12ca7b19ed1fba73f73725
Jeremy Orellana <jejorm8@gmail.com>
2026-04-07 15:10:00 -0500

    feat: add islands page with persistent counter component and update layout navigation

--- src/components/shared/Counter.tsx
+++ src/components/shared/Counter.tsx
@@ -1,15 +1,32 @@
-import { createSignal } from 'solid-js'
+import { createSignal, type JSX } from 'solid-js'
 
-export const Counter = () => {
+interface Props {
+  initValue?: number
+  children?: JSX.Element
+}
 
-  const [count, setCount] = createSignal(10)
+export const Counter = ({ initValue = 10, children }: Props) => {
+
+  const [count, setCount] = createSignal(initValue)
 
   return (
     <>
-      <h1 class='text-5xl my-4'>Counter: {count()}</h1>
+      {/* <h1 class='text-5xl my-4'>Counter: {count()}</h1>
+       */}
+      {children}
 
-      <button class='bg-blue-500 p-2 mr-2 rounded cursor-pointer' onClick={() => setCount(prev => ++prev)}>+1</button>
-      <button class='bg-blue-500 p-2 mr-2 rounded cursor-pointer' onClick={() => setCount(prev => --prev)}>-1</button>
+      <button
+        class="bg-blue-500 p-2 mr-2 rounded cursor-pointer"
+        onClick={() => setCount((prev) => ++prev)}
+      >
+        +1
+      </button>
+      <button
+        class="bg-blue-500 p-2 mr-2 rounded cursor-pointer"
+        onClick={() => setCount((prev) => --prev)}
+      >
+        -1
+      </button>
     </>
   )
 }
